35e9291b99e0a091a72982204e95e3578dfa94eb
BlackSun559 <blacksun559@gmail.com>
2018-01-29 14:58:40 -0600

    Initial Push
    Basic afk clicker for Cookie Clicker

---
+++ BlackSun559Clicker.js
@@ -0,0 +1,23 @@
+var AutoPlay;
+
+Autoplay.autoPlayer = setInterval(Autoplay.run, 300);
+
+Autoplay.run = function()
+{
+	Autoplay.handleGoldenCookies();
+	Autoplay.handleClicking();
+}
+
+Autoplay.handleGoldenCookies = function()
+{
+	for(sx in Game.shimmers) {
+    var s=Game.shimmers[sx];
+    if((s.type!="golden") || (s.life<Game.fps) || (!Game.Achievements["Early bird"].won)) { s.pop(); return; }
+    if((s.life/Game.fps)<(s.dur-2) && (Game.Achievements["Fading luck"].won)) { s.pop(); return; }
+	}
+}
+
+Autoplay.handleClicking = function()
+{
+	Game.ClickCookie();
+}
